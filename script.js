e594bc6990cbe67d36ea3c858da18e85057a91e7
Khushighosh <khushighosh371@gmail.com>
2026-05-20 14:08:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio website loaded");
+
+// Simple alert on form submit
+document.querySelector("form").addEventListener("submit", function () {
+    alert("Message sent successfully!");
+});
